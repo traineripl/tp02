383a3ee8233cc9ad8a8c8f558fd64ca51b37f7d9
mohamedgader <it.leaders.pioneers@gmail.com>
2026-06-11 14:12:42 +0100

    fix: puppeteer CI sandbox issue

--- tests/a11y.audit.js
+++ tests/a11y.audit.js
@@ -1,10 +1,11 @@
 const puppeteer = require("puppeteer");
 
 (async () => {
-  const browser = await puppeteer.launch({
-    headless: true
-  });
-
+const browser = await puppeteer.launch({
+  headless: "new",
+  executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
+  args: ["--no-sandbox", "--disable-setuid-sandbox"]
+});
   const page = await browser.newPage();
 
   await page.goto("file://" + __dirname + "/../index.html");
